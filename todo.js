93f48e312d4c1e19ec95a52ce0611a5347501423
MannJefferson <mannjefferson84@gmail.com>
2022-09-28 16:33:20 +0200

    toDo4

--- todo.js
+++ todo.js
@@ -1,50 +1,42 @@
-// Task Class: Represents a Task
+// Task Class: indicates a toDo
 class Task {
-    constructor(title, date) { // a method that runs when creating a Task
-        this.title = title;
-        this.date = date;
-        
-    }
-}
-//UI Class: Handle UI Tasks
-class UI {
+    constructor(title, date) {
+      this.title = title;
+      this.date = date;
+      }
+  }
+  
+  // UI Class: Handle UI Tasks
+  class UI {
     static displayTasks() {
-        const StoredTasks = [
-            {
-                title: 'Check Emails',
-                date: '25-08-2022',
-                
-            },
-            {
-                title: 'Return Calls',
-                date: '30-09-2022',
-                },
-
-        ];
-        const tasks = StoredTasks;
-        // to add task/to do to the table list
-        tasks.forEach((task) => UI.addTaskToList(task));
-
+      const tasks = Store.getTasks();
+  
+      tasks.forEach((task) => UI.addTaskToList(task));
     }
+  
     static addTaskToList(task) {
-        const list = document.querySelector('#todo-list');
-
-        const row = document.createElement('tr');
-        row.innerHTML = `
-        
+      const list = document.querySelector('#todo-list');
+  
+      const row = document.createElement('tr');
+  
+      row.innerHTML = `
         <td>${task.title}</td>
         <td>${task.date}</td>
         <td><a href="#" class="btn btn-primary btn-sm complete">complete</a></td>
         <td><a href="#" class="btn btn-warning btn-sm delete">delete</a></td>
-       `;
-        list.appendChild(row);
+      `;
+  
+      list.appendChild(row);
     }
+  
     static deleteTask(el) {
-        if (el.classList.contains('delete')) {
-            el.parentElement.parentElement.remove();
-        }
+      if(el.classList.contains('delete')) {
+        el.parentElement.parentElement.remove();
+      }
     }
-    //  to add completion function on Task and to change button from complete to done.
+  
+
+
     static completeTask(el) {
         if (el.classList.contains('complete')) {
             el.parentElement.parentElement.style.setProperty("text-decoration", "line-through");
@@ -52,48 +44,109 @@ class UI {
             //.textContent='redo';
         }
            }
-static buttonupdate(el){
-  if (el.classList.contains('complete')) {
-    document.querySelector('#btn btn-primary btn-sm complete').innertext="done";
- }
-}
 
+
+
+
+    static showAlert(message, className) {
+      const div = document.createElement('div');
+      div.className = `alert alert-${className}`;
+      div.appendChild(document.createTextNode(message));
+      const container = document.querySelector('.container');
+      const form = document.querySelector('#todo-form');
+      container.insertBefore(div, form);
+  
+      // Vanish in 3 seconds
+      setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+  
     static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#date').value = '';
-            }
-}
-//Store Class: Handles Storages
-// Event: Display Tasks
-document.addEventListener('DOMContentLoaded', UI.displayTasks);
-// Event: Add a Task
-document.querySelector('#todo-form').addEventListener('submit', (e) => {
-    //prevent actual submit
+      document.querySelector('#title').value = '';
+      document.querySelector('#date').value = '';
+     }
+  }
+  
+  // Store Class: Handles Storage
+  class Store {
+    static getTasks() {
+      let tasks;
+      if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+      } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+      }
+  
+      return tasks;
+    }
+  
+    static addTask(task) {
+      const tasks = Store.getTasks();
+      tasks.push(task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  
+    static removeTask(date) {
+      const tasks = Store.getTasks();
+  
+      tasks.forEach((date, index) => {
+        if(task.date === date) {
+          tasks.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  }
+  
+  // Event to display toDo tasks
+  document.addEventListener('DOMContentLoaded', UI.displayTasks);
+  
+  // Event: to Add a task / Todo
+  document.querySelector('#todo-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
     e.preventDefault();
-
-    //Get form Values
+  
+    // Get form values
     const title = document.querySelector('#title').value;
     const date = document.querySelector('#date').value;
     
-    //Instantiate Task
-    const task = new Task(title, date);
-    console.log(task);
+  
+    // Validate
+    if(title === '' || date === '') {
+      UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+      // Instatiate toDo
+      const task = new Task(title, date);
+  
+      // Adding toDo to UI
+      UI.addTaskToList(task);
+  
+      // Adding toDo to store
+      Store.addTask(task);
+  
+      // Show success message
+      UI.showAlert('ToDo Added', 'success');
+  
+      // Clear fields
+      UI.clearFields();
+    }
+  });
+  
+  // Event: Remove a toDo or task
+  document.querySelector('#todo-list').addEventListener('click', (e) => {
+    // Remove toDo from UI
+    UI.deleteTask(e.target);
 
-    //Add Task to UI
-    UI.addTaskToList(task);
-    //Clear Fields
-    UI.clearFields();
-});
 
-//Events:Remove a Task
-document.querySelector('#todo-list').addEventListener('click', (e) => {
-    UI.deleteTask(e.target)
-});
-document.querySelector('#todo-list').addEventListener('click', (e) => {
-    UI.completeTask(e.target)
-});
+    document.querySelector('#todo-list').addEventListener('click', (e) => {
+        UI.completeTask(e.target)
+    });
 
-// to change the button once task has been completed
-document.querySelector('#todo-list').addEventListener('click', (e) => {
-    UI.buttonupdate(e.target)
-});
+
+  
+    // Remove toDO from store
+    Store.removeTask(e.target.parentElement.previousElementSibling.textContent);
+  
+    // Show success message
+    UI.showAlert('toDO Removed', 'success');
+  });
